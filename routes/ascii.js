f33faddbaa6e811797d2f6730464a3d098e0ff7d
edickins <bleepbloop@bleepbloop.net>
2020-11-21 19:29:54 +0000

    added ascii content

--- routes/ascii.js
+++ routes/ascii.js
@@ -3,8 +3,6 @@ var router = express.Router();
 const generator = require("../modules/asciiGenerator.js");
 router.get("/", function (req, res, next) {
   const initObj = req.body;
-  return new Promise(function (resolve, reject) {
-    resolve(generator.getText());
-  });
+  res.json(generator.getText());
 });
 module.exports = router;
